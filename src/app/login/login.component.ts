aed60ca2a5690f08a8684a38e3d95d542836f824
Michael Majdanski <michael.majdanski@gmail.com>
2018-02-01 17:48:13 -0800

    Changing LoginService to use onAuthStateChanged

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -19,10 +19,25 @@ export class LoginComponent implements OnInit {
   constructor(public afAuth: AngularFireAuth, private loginService: LoginService, public route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.route.data.subscribe((data) => {
+    this.afAuth.auth.onAuthStateChanged((user) => {
+      if (user) {
+        
+        // User is signed in.
+        this.displayName = user.displayName;
+        // ...
+      } else {
+        // User is signed out.
+        // ...
+      }
+    });
+    
+
+
+    /*this.route.data.subscribe((data) => {
+      console.log(data);
       this.displayName = data.displayName;
       this.setLoginServiceDisplayName(data.displayName);
-    });
+    });*/
   }
 
   loginAnon(displayName) {
